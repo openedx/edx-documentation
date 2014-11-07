cd06deaec5e6a1a0340c65b38f6801927699b08f
Mark Hoeber <hoeber@edx.org>
2014-11-06 20:47:21 -0500

    changed conf to point to theme

--- en_us/course_authors/source/conf.py
+++ en_us/course_authors/source/conf.py
@@ -6,16 +6,9 @@ import sys, os
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'edx_theme'
-    html_theme_path = ["../../.."]
 
-
-
-
-
-
+html_theme = 'edx_theme'
+html_theme_path = ["../../.."]
 
 
 sys.path.append(os.path.abspath('../../../'))
