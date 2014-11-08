a38ead02e5226952daa843e16992ef0ccc365776
Mark Hoeber <hoeber@edx.org>
2014-11-07 19:44:58 -0500

    revert to theme import

--- en_us/release_notes/source/conf.py
+++ en_us/release_notes/source/conf.py
@@ -23,25 +23,25 @@ master_doc = 'index'
 #html_static_path.append('source/_static')
 
 
-#html_theme = 'edx_theme'
-#html_theme_path = ["../.."]
+html_theme = 'edx_theme'
+html_theme_path = ["../.."]
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    #html_theme = 'edx_theme'
-    #html_theme_path = ["../.."]
+    #html_theme = 'sphinx_rtd_theme'
+    #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme = 'edx_theme'
+    html_theme_path = ["../.."]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["."]
-html_static_path.append('_static')
+#html_static_path = ["."]
+#html_static_path.append('_static')
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["."]
-templates_path.append('_templates')
+#templates_path = ["."]
+#templates_path.append('_templates')
 
 
 # General information about the project.
