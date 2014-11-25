c5e200880729c29a2d7043f48c3591c8e3500f78
Mark Hoeber <hoeber@edx.org>
2014-11-25 09:23:47 -0500

    test

--- en_us/release_notes/source/conf.py
+++ en_us/release_notes/source/conf.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('.'))
 
 master_doc = 'index'
-html_theme = 'pyramid'
+#html_theme = 'pyramid'
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path.append('source/_templates')
@@ -24,8 +24,8 @@ html_theme = 'pyramid'
 #html_static_path.append('source/_static')
 
 
-#html_theme = 'edx_theme'
-#html_theme_path = ["_themes"]
+html_theme = 'edx_theme'
+html_theme_path = ["_themes"]
 #html_static_path = ['_themes/edx_theme/static']
 #templates_path = ['_templates']
 
