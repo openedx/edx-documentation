aaf9f77af321787f3c6796e539353cc76fcf218a
Mark Hoeber <hoeber@edx.org>
2014-12-19 11:40:42 -0500

    static folder test

--- en_us/release_notes/source/conf.py
+++ en_us/release_notes/source/conf.py
@@ -4,8 +4,18 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 extensions = [
              ]
+
+
+sys.path.append(os.path.abspath('../../../'))
+sys.path.append(os.path.abspath('../../'))
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -20,7 +30,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'edX Course Staff Release Notes'
+project = u'Release Notes'
 copyright = u'2014, edX'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -41,7 +51,7 @@ release = ''
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'edx_theme'
+# html_theme = 'edx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -49,7 +59,7 @@ html_theme = 'edx_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = ['_themes']
+#html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -70,7 +80,7 @@ html_theme_path = ['_themes']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_static/scrape_logo2.png',]
+html_static_path = ['_static', '_static/theme.css', '_static/edx-logo-header.png']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -101,8 +111,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Scrape.tex', u'Scrape Documentation',
-   u'Yarko Tymciurak', 'manual'),
+  
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
