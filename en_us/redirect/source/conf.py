649ca15375aa2b6b10d498292ea42e5d57cb729a
Peter Desjardins <pdesjardins@edx.org>
2015-12-22 14:51:40 -0500

    Added a test document that redirects to docs.edx.org.

---
+++ en_us/redirect/source/conf.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import sys, os
+
+sys.path.append('../../../')
+
+from shared.conf import *
+
+html_theme = 'edx_theme'
+
+html_theme_path = ['../../_themes']
+
+html_favicon = '../../_themes/edx_theme/static/css/favicon.ico'
+
+project = u'edX Learner\'s Guide'
+
+templates_path = ["templates"]
+
+tags.add('Partners')
+
+product = 'Partners'
+
+def setup(app):
+    app.add_config_value('product', '', True)
