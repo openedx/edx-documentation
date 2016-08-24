9482c716c3d0f69ee6aac60f6732a92fe81030d0
Alison Hodges <ahodges@edx.org>
2016-08-24 12:27:04 -0400

    Updates guide names to include Eucalyptus

--- en_us/open_edx_students/source/conf.py
+++ en_us/open_edx_students/source/conf.py
@@ -11,7 +11,7 @@ html_theme_path = ['../../_themes']
 
 html_favicon = '../../_themes/edx_theme/static/css/favicon.ico'
 
-project = u'Open edX Learner\'s Guide'
+project = u'Open edX Learner\'s Guide: Eucalyptus Release'
 
 exclude_patterns = ['links.rst', 'reusables/*', 'SFD_mathformatting.rst']
 
