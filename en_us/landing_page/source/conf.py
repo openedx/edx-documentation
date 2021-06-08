c344ef990cdc70a4773944bc70f8e073f9682bdf
Feanil Patel <feanil@edx.org>
2021-06-08 13:59:04 -0400

    chore: Run make upgrade.
    Update landing_page/sourc/conf.py to user the new sphinx function for
    adding js to a sphinx page.

--- en_us/landing_page/source/conf.py
+++ en_us/landing_page/source/conf.py
@@ -8,6 +8,6 @@ from shared.conf import *
 project = u'EdX Documentation Resources'
 
 
-def setup(app): app.add_javascript('edx_js.js')
+def setup(app): app.add_js_file('edx_js.js')
 
 html_static_path = ['_static/edx_js.js']
