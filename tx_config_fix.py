102a9d4431d3f7fac1dc9b5f32ace44dea2dbccc
Carlos Muniz <cmuniz@trcil.org>
2022-03-21 14:12:04 -0400

    fix: Make get more pythonic with fallback
    In addition, the script was reformatted as it all falls under the
    function now.

--- tx_config_fix.py
+++ tx_config_fix.py
@@ -1,18 +1,3 @@
-"""
-This script changes the section titles of the tx config file to be more unique based on
-the directory of the file that is used to make the original section title.
-
-The script loops through the sections of the tx config and changes the sections that
-match the project_path argument variable. The script then adds a delimited version of the
-directory to the section title making the title more unique. These section titles become
-the file names on Transifex, and if they are not unique, they overwrite eachother.
-
-example:
-If we had a file "example/index.rst" that we wanted to translate, in tx config we would
-have a section with the title [index] and the source_file: "example/index.rst". But this
-filename is common in Sphinx Projects. This script would rename the section title to be
-[example--title], keeping all other values under it untouched.
-"""
 from configparser import ConfigParser
 
 import click
@@ -20,22 +5,34 @@ import click
 
 @click.command()
 @click.argument("project_path")
-def delimit_path(project_path):
-    return "--".join(project_path.split("/"))
+def delimit_sections(project_path):
+    """
+    This script changes the section titles of the tx config file to be more unique based
+    on the directory of the file that is used to make the original section title.
 
+    The script loops through the sections of the tx config and changes the sections that
+    match the project_path argument variable. The script then adds a delimited version of
+    the directory to the section title making the title more unique. These section titles
+    become the file names on Transifex, and if they are not unique, they overwrite
+    eachother.
 
-project_path_friendly = delimit_path()
+    Example:
+    If we had a file "example/index.rst" that we wanted to translate, in tx config we
+    would have a section with the title [index] and the source_file: "example/index.rst".
+    But this filename is common in Sphinx Projects. This script would rename the section
+    title to be [example--title], keeping all other values under it untouched.
+    """
 
-config_path = "./.tx/config"
+    project_path_friendly = "--".join(project_path.split("/"))
 
-config = ConfigParser()
-config.read(config_path)
+    config_path = "./.tx/config"
 
+    config = ConfigParser()
+    config.read(config_path)
 
-for section_title in config.sections():
-    if config.has_option(section_title, "source_file"):
-        source_file_path = config.get(section_title, "source_file")
-        if source_file_path[: len(project_path)] == project_path:
+    for section_title in config.sections():
+        source_file_path = config.get(section_title, "source_file", fallback=None)
+        if source_file_path and source_file_path.startswith(project_path):
             split_section_title = section_title.split(".")
             split_section_title[1] = project_path_friendly + split_section_title[1]
             new_section_title = ".".join(split_section_title)
@@ -46,5 +43,9 @@ for section_title in config.sections():
                 )
             config.remove_section(section_title)
 
-with open(config_path, "w") as file:
-    config.write(file)
+    with open(config_path, "w") as file:
+        config.write(file)
+
+
+if __name__ == "__main__":
+    delimit_sections()
