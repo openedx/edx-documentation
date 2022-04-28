b3fd9b77a1110a86f5cf6bdae8db60fb52ef3dd7
owls-doc <104441405+owls-doc@users.noreply.github.com>
2022-04-28 10:06:17 -0400

    Changing project to MITx

--- en_us/course_authors/source/conf.py
+++ en_us/course_authors/source/conf.py
@@ -6,7 +6,7 @@ sys.path.append('../../../')
 from shared.conf import *
 from shared.edxconf import *
 
-project = u'Building and Running an edX Course'
+project = u'Building and Running an MITx Online Course'
 
 tags.add('Partners')
 set_audience(PARTNER, COURSE_TEAMS)
