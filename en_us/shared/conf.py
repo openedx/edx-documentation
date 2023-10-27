3431d16e69db393632bed265120a09c98b8b904e
Sarina Canelake <sarina@axim.org>
2023-10-27 12:48:34 -0400

    build: Update browser redirect

--- en_us/shared/conf.py
+++ en_us/shared/conf.py
@@ -6,5 +6,5 @@ sys.path.append("../../../")
 from shared.conf import *
 
 redirects = {
-    "browsers.rst": "https://docs.openedx.org/en/latest/developers/references/developer_guide/front_matter/browsers.html",
+    "browsers.rst": "https://docs.openedx.org/en/latest/developers/references/developer_guide/testing/browsers.html",
 }
