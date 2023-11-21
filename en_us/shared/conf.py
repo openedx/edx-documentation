2b435598378f481142e251b769917bdea8d8498d
JoeMartis <jmartis@mit.edu>
2023-11-21 08:28:57 -0500

    Update conf.py
    adding shared.edxconf here

--- en_us/shared/conf.py
+++ en_us/shared/conf.py
@@ -3,7 +3,8 @@ import sys
 
 sys.path.append("../../../")
 
-from shared.conf import *
+    from shared.conf import *
+    from shared.edxconf import *
 
 redirects = {
     "browsers.rst": "https://docs.openedx.org/en/latest/developers/references/developer_guide/testing/browsers.html",
