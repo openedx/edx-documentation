c632c72996741e70ee449c78a96b91532eca2150
JoeMartis <jmartis@mit.edu>
2023-11-28 10:29:09 -0500

    Update edxconf.py
    swapping logos

--- shared/edxconf.py
+++ shared/edxconf.py
@@ -8,5 +8,5 @@ Import this into conf.py after the common configuration::
 
 """
 
-html_logo = "https://www.edx.org/images/logos/edx-logo-elm.svg"
-html_favicon = "https://www.edx.org/favicon.ico"
+html_logo = "https://edxuploads.s3.amazonaws.com/organization_logos/2a73d2ce-c34a-4e08-8223-83bca9d2f01d-239e27d970f5.png"
+html_favicon = "https://mitxonline.mit.edu/static/images/favicon.ico"
